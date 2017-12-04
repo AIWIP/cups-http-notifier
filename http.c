f167adb8b35abb418d0b79dda777d3288161783c
James Campbell <james@supmenow.com>
2017-12-04 18:00:15 +0000

    Pass in integer.

--- http.c
+++ http.c
@@ -62,56 +62,17 @@
         json_object_object_add(request_json, ippTagString(group), group_json);
       }
 
-      ippAttributeString(attr, buffer, sizeof(buffer));
-
-
-      // IPP_TAG_CUPS_INVALID = -1,		/* Invalid tag name for @link ippTagValue@ */
-      // IPP_TAG_OPERATION,			/* Operation group */
-      // IPP_TAG_JOB,				/* Job group */
-      // IPP_TAG_END,				/* End-of-attributes */
-      // IPP_TAG_PRINTER,			/* Printer group */
-      // IPP_TAG_UNSUPPORTED_GROUP,		/* Unsupported attributes group */
-      // IPP_TAG_SUBSCRIPTION,			/* Subscription group */
-      // IPP_TAG_EVENT_NOTIFICATION,		/* Event group */
-      // IPP_TAG_RESOURCE,			/* Resource group @private@ */
-      // IPP_TAG_DOCUMENT,			/* Document group */
-      // IPP_TAG_UNSUPPORTED_VALUE = 0x10,	/* Unsupported value */
-      // IPP_TAG_DEFAULT,			/* Default value */
-      // IPP_TAG_UNKNOWN,			/* Unknown value */
-      // IPP_TAG_NOVALUE,			/* No-value value */
-      // IPP_TAG_NOTSETTABLE = 0x15,		/* Not-settable value */
-      // IPP_TAG_DELETEATTR,			/* Delete-attribute value */
-      // IPP_TAG_ADMINDEFINE,			/* Admin-defined value */
-      // IPP_TAG_BOOLEAN,			/* Boolean value */
-      // IPP_TAG_ENUM,				/* Enumeration value */
-      // IPP_TAG_STRING = 0x30,		/* Octet string value */
-      // IPP_TAG_DATE,				/* Date/time value */
-      // IPP_TAG_RESOLUTION,			/* Resolution value */
-      // IPP_TAG_RANGE,			/* Range value */
-      // IPP_TAG_BEGIN_COLLECTION,		/* Beginning of collection value */
-      // IPP_TAG_TEXTLANG,			/* Text-with-language value */
-      // IPP_TAG_NAMELANG,			/* Name-with-language value */
-      // IPP_TAG_END_COLLECTION,		/* End of collection value */
-      // IPP_TAG_TEXT = 0x41,			/* Text value */
-      // IPP_TAG_NAME,				/* Name value */
-      // IPP_TAG_RESERVED_STRING,		/* Reserved for future string value @private@ */
-      // IPP_TAG_KEYWORD,			/* Keyword value */
-      // IPP_TAG_URI,				/* URI value */
-      // IPP_TAG_URISCHEME,			/* URI scheme value */
-      // IPP_TAG_CHARSET,			/* Character set value */
-      // IPP_TAG_LANGUAGE,			/* Language value */
-      // IPP_TAG_MIMETYPE,			/* MIME media type value */
-      // IPP_TAG_MEMBERNAME,			/* Collection member name value */
-      // IPP_TAG_EXTENSION = 0x7f,		/* Extension point for 32-bit tags */
-
-      // Integer
-      if (attr->value_tag == IPP_TAG_INTEGER)
+      // Integer, Boolean or Enum
+      if (attr->value_tag == IPP_TAG_INTEGER ||
+          attr->value_tag == IPP_TAG_BOOLEAN ||
+          attr->value_tag == IPP_TAG_ENUM )
       {
-        json_value = json_object_new_int (buffer);
+        json_value = json_object_new_int(attr->value);
       }
 
       // All Other Values
       else
+        ippAttributeString(attr, buffer, sizeof(buffer));
         json_value = json_object_new_string(buffer);
       }
       
